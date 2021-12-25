8823be347b8181d2a27054a2a7788009dcb7479b
DanielGodoyD <96601659+DanielGodoyD@users.noreply.github.com>
2021-12-25 12:15:53 -0500

    Add files via upload

---
+++ menu_cti.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/miestilo.css">
+		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+		<script src="script.js"></script>
+		<title> Menu de opciones</title>
+	</head>
+	<body>
+		<header>
+			<div id="banner" style="text-align:center;">
+				Sistema de Gestión de Proyectos
+			</div>
+		</header>
+		<header  style="background-image: url('https://southamericanoutdoors.com/wp-content/uploads/2016/11/macaws.jpg');">
+    		<div>
+			<img src="img/logo.jpg" alt="" align="center">
+			</div>
+		  </header>
+		
+
+
+		<div id='cssmenu'>
+			<ul>
+				<li><a href='#'>Inicio</a>
+				</li>
+				
+				
+				<li><a href='#'>Evaluación de Fases</a>
+				    <ul>
+						<li><a href='donatario/fase1.php'>Fase 0</a>								
+						</li>
+						
+						<li><a href='fase2/fase2.php'>Fase 2</a>								
+						</li>
+						
+					</ul>					
+			    </li>
+	
+				<li><a href='login/index.html'>Salir del Sistema</a>
+			    </li>
+			</ul>
+		</div>
+	</body>
+
+
+<h1 style="text-align:center;">¡Este es el nuevo Sistema de Vive Amazonas!</h1>
+
+</html>
---
+++ menu_dige.php
@@ -0,0 +1,74 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/miestilo.css">
+		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+		<script src="script.js"></script>
+		<title> Menu de opciones</title>
+	</head>
+	<body>
+		<header>
+			<div id="banner" style="text-align:center;">
+				Sistema de Gestión de Proyectos
+			</div>
+		</header>
+		<header  style="background-image: url('https://southamericanoutdoors.com/wp-content/uploads/2016/11/macaws.jpg');">
+    		<div>
+			<img src="img/logo.jpg" alt="" align="center">
+			</div>
+		  </header>
+		
+
+
+		<div id='cssmenu'>
+			<ul>
+				<li><a href='#'>Inicio</a>
+				</li>
+		
+				<li class='active'><a href='#'>Gestión 2</a>
+				         <ul>
+                            
+							
+							<li><a href='comite/comite.php'>Gestión de Comité</a>
+													 
+							</li>	
+							<li><a href='vincularComite/vincularComite.php'>Vincular Comité</a>
+													 
+							</li>		
+							
+							
+						</ul>
+				</li>
+				<li><a href='#'>Evaluación de Fases</a>
+				    <ul>
+						
+						<li><a href='fase1/fase_1.php'>Fase 1</a>								
+						</li>		
+						
+						<li><a href='subsanacion/subsanacion.php'>Subsanacion</a>								
+						</li>
+					
+					</ul>					
+			    </li>
+				<li class='active'><a href='#'>Concursos</a>
+					<ul>
+
+						   <li><a href='concursos/concursos.php'>Gestión de Concursos</a>
+                            
+							</li>
+						
+					</ul>
+				</li>
+	
+				<li><a href='login/index.html'>Salir del Sistema</a>
+			    </li>
+			</ul>
+		</div>
+	</body>
+
+
+<h1 style="text-align:center;">¡Este es el nuevo Sistema de Vive Amazonas!</h1>
+
+</html>
